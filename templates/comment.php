13a22d883acd379037a5d64d8e7c346d74ce3ad5
Daniel Bachhuber <d@danielbachhuber.com>
2013-05-01 08:31:35 -0700

    To consolidate the email templates, add the permalink around the date

--- templates/comment.php
+++ templates/comment.php
@@ -4,4 +4,4 @@
 
 <?php comment_text( $comment->ID ); ?>
 
-<p style="color:grey;"><?php comment_author( $comment->ID ); ?> | <?php echo sprintf( '%s at %s', get_comment_time( get_option( 'date_format' ) ), get_comment_time( get_option( 'time_format' ) ) ); ?> | <a href="<?php echo esc_url( get_comment_link( $comment->ID ) ); ?>">Permalink</a></p>
+<p style="color:grey;"><?php comment_author( $comment->ID ); ?> | <a href="<?php echo esc_url( get_comment_link( $comment->ID ) ); ?>"><?php echo sprintf( '%s at %s', get_comment_time( get_option( 'date_format' ) ), get_comment_time( get_option( 'time_format' ) ) ); ?></a></p>
